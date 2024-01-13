0994b241885cda3039fb781bdc1b8ab6ea08890a
Tasleemofx <oladepotesleem5@gmail.com>
2024-01-13 12:11:54 +0100

    contact page fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    EMAILJS_KEY: process.env.EMAILJS_KEY,
+
+    SERVICE_ID: process.env.SERVICE_ID,
+     TEMPLATE_ID: process.env.TEMPLATE_ID
+  }
+
 }
 
 module.exports = nextConfig
--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -1,34 +1,53 @@
+"use client"
+import { useRef, useState } from "react";
+import  {config} from "dotenv"
 import emailjs from "@emailjs/browser";
 import styles from "./styles/contact.module.css";
 
 
 const Contact = () => {
- function submitForm (event){
-  event.preventDefault()
-    this.contact_number.value = Math.random() * 100000 | 0;
-    // these IDs from the previous steps
-    emailjs.sendForm('contact_service', 'contact_form', this)
-      .then(function () {
-        console.log('SUCCESS!');
-      }, function (error) {
-        console.log('FAILED...', error);
+  const [messageStatus, setMessageStatus] = useState("")
+  config()
+  const formdetails = useRef()
+
+  function submitForm(e){
+    console.log(process.env.EMAILJS_KEY)
+    console.log(process.env.TEMPLATE_ID)
+    console.log(process.env.SERVICE_ID)
+    console.log(formdetails.current.name.value)
+    console.log(formdetails.current.email.value)
+    console.log(formdetails.current.message.value)
+    e.preventDefault();
+    emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, formdetails.current, process.env.EMAILJS_KEY)
+      .then((result) => {
+        setMessageStatus(result.text);
+        formdetails.current = {}
+      }, (error) => {
+        setMessageStatus(error.text);
       });
- }
+  }
   return (
 
     <main className={styles.contactform}>  
-      <form onSubmit={submitForm}>
-        <input type="hidden" name="contact_number" />
+    {messageStatus && <p className={styles.status}>Message sent</p>}
+    <h2 className={styles.contactformhead}>Contact Me</h2>
+      <form onSubmit={submitForm} ref={formdetails}>
+     
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="name"required className={styles.inp}/>
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="email" required className={styles.inp}/>
         <label>Message</label>
-        <textarea name="message"></textarea>
-        <input type="submit" value="Send" />
+        <textarea name="message" className={styles.txarea} minLength={10}></textarea>
+        <input type="submit" value="Send" className={styles.submitbtn}/>
       </form>
     </main>
   )
 }
 
 export default Contact
+
+
+// export async function getStaticProps(){
+
+// }
